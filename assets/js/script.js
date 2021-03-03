58522ef9932519d643226dfa88fdbd4fb6381c51
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-03-03 10:27:06 -0800

    changed h4 tags to div tags and input tags to textarea tags; changed col sizes from 2 to 1 and 8 to 10; deleted images folder in assets folder and inserted i tag from fontawesome in the button tags instead for the save image; removed input CSS selector; begun event listener for save buttons on each timeblock; commented out original function I was attempting for chenging the colors because I am going to try something else.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,42 @@
 const today = moment();
 $("#currentDay").text(today.format("dddd MMMM Do"));
-console.log(today);
+let rowHour;
+let currentHour = parseInt(today.format("H"));
+console.log(currentHour);
+let rowIdStr;
+let input; 
+let rows = $(".row");
+let button = $(".saveBtn");
+let h4 = $(".hour")
+console.log(rows);
+ 
+
+
+
+
+// Array.from(rows).forEach(function(row) {
+//     rowIdStr = row.id;
+//     console.log(row);
+//     console.log(rowIdStr);
+    
+//     if (rowIdStr) {
+//         rowHour = parseInt(rowIdStr);
+//         console.log(rowHour);
+//     }
+//     if (rowHour) {
+//         if ((currentHour > rowHour) && (currentHour < rowHour + 9)) {
+//             $("input").attr("class", "col-8 past");
+//         } else if (currentHour === rowHour) {
+//            $("input").attr("class", "col-8 present");
+//         } else if ((currentHour < rowHour) && (currentHour > rowHour - 9)) {
+//             $("input").attr("class", "col-8 future")
+//         }    
+//     }
+// })
+
+
+button.on("click", function(event) {
+    console.log(event);
+    // let store = input.value
+    // localStorage.setItem(, store);     
+})
