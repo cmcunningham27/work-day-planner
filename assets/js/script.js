ef85ee424b5fba5e120a7c69dda0f9950371a0d9
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-03-03 11:47:25 -0800

    created onclick function for the rows' buttons to collect the value (time) of button's sibling with the input id and gets the id of the button's parent element; then it stores the information in the user's local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ let currentHour;
 
 let rowHour;
 let rowIdStr;
-let input; 
 let rows = $(".row");
 let button = $(".saveBtn");
 let h4 = $(".hour")
@@ -27,30 +26,8 @@ setInterval(function(){
 }, 1000);
 
 
-
-// Array.from(rows).forEach(function(row) {
-//     rowIdStr = row.id;
-//     console.log(row);
-//     console.log(rowIdStr);
-    
-//     if (rowIdStr) {
-//         rowHour = parseInt(rowIdStr);
-//         console.log(rowHour);
-//     }
-//     if (rowHour) {
-//         if ((currentHour > rowHour) && (currentHour < rowHour + 9)) {
-//             $("input").attr("class", "col-8 past");
-//         } else if (currentHour === rowHour) {
-//            $("input").attr("class", "col-8 present");
-//         } else if ((currentHour < rowHour) && (currentHour > rowHour - 9)) {
-//             $("input").attr("class", "col-8 future")
-//         }    
-//     }
-// })
-
-
 button.on("click", function() {
-    console.log(this);
-    // let store = input.value
-    // localStorage.setItem(, store);     
-})
+    let text = $(this).siblings("#input").val();
+    let time = $(this).parent().attr("id");
+    localStorage.setItem(time, text);  
+});
